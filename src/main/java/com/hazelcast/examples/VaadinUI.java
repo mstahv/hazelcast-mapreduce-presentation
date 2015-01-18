8034b1847358106fd855e0d1205f92edc2fa220a
Matti Tahvonen <matti@vaadin.com>
2015-01-18 19:44:01 +0200

    Added workaround for FF + Responsive

--- src/main/java/com/hazelcast/examples/VaadinUI.java
+++ src/main/java/com/hazelcast/examples/VaadinUI.java
@@ -4,6 +4,9 @@ import com.vaadin.annotations.JavaScript;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.cdi.CDIUI;
+import com.vaadin.server.Extension;
+import com.vaadin.server.Page;
+import com.vaadin.server.Responsive;
 import org.vaadin.cdiviewmenu.ViewMenuUI;
 
 @Theme("valo")
@@ -11,5 +14,21 @@ import org.vaadin.cdiviewmenu.ViewMenuUI;
 @JavaScript("prettify.js")
 @Title("MapReduce tutorial")
 public class VaadinUI extends ViewMenuUI {
+
+    @Override
+    public void beforeClientResponse(boolean initial) {
+        if (initial && Page.getCurrent().getWebBrowser().getBrowserApplication().
+                contains("Firefox")) {
+            // Responsive, FF, cross site is currently broken :-(
+            Extension r = null;
+            for (Extension ext : getExtensions()) {
+                if (ext instanceof Responsive) {
+                    r = ext;
+                }
+            }
+            removeExtension(r);
+        }
+        super.beforeClientResponse(initial);
+    }
 
 }
